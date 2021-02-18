eeed7fac6d3fdce47c6c810b0a900091580b2579
Anatolii Gurskyi <anatolii.gurskyi@avenga.com>
2021-02-18 19:14:24 +0200

    [LTS-102] Error Handling

--- src/main/java/com/avenga/fil/lt/service/ResponseService.java
+++ src/main/java/com/avenga/fil/lt/service/ResponseService.java
@@ -1,10 +1,11 @@
 package com.avenga.fil.lt.service;
 
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
+import com.avenga.fil.lt.data.Status;
 
 public interface ResponseService {
 
     APIGatewayProxyResponseEvent createErrorResponse(Exception exception);
 
-    APIGatewayProxyResponseEvent createSuccessResponse(String message);
+    APIGatewayProxyResponseEvent createSuccessResponse(Status status);
 }
