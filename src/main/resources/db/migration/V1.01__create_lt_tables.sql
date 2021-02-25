cc04d7fb55393e9e856bf337defb102143b5137f
Anatolii Gurskyi <anatolii.gurskyi@avenga.com>
2021-02-25 10:16:12 +0200

    [LTS-39] Change table and column names to lower underscore

--- src/main/resources/db/migration/V1.01__create_lt_tables.sql
+++ src/main/resources/db/migration/V1.01__create_lt_tables.sql
@@ -1,17 +1,17 @@
-CREATE TABLE LT_EXTRACT_DATA
+CREATE TABLE lt_extract_data
 (
-    LT_EXTRACT_DATA_KEY SERIAL PRIMARY KEY,
-    USER_ID             VARCHAR(255) NOT NULL,
-    DOCUMENT_NAME       VARCHAR(255) NOT NULL,
-    PAGE_COUNT          INT4         NOT NULL DEFAULT 1,
-    EXTRACT_DATE_TIME   TIMESTAMP    NOT NULL
+    lt_extract_data_key SERIAL PRIMARY KEY,
+    user_id             VARCHAR(255) NOT NULL,
+    document_name       VARCHAR(255) NOT NULL,
+    page_count          INT4         NOT NULL DEFAULT 1,
+    extract_date_time   TIMESTAMP    NOT NULL
 );
 
-CREATE TABLE LT_TRANSLATE_DATA
+CREATE TABLE lt_translate_data
 (
-    LT_TRANSLATE_DATA_KEY SERIAL PRIMARY KEY,
-    USER_ID               VARCHAR(255) NOT NULL,
-    DOCUMENT_NAME         VARCHAR(255) NOT NULL,
-    TEXT_LENGTH           INT4         NOT NULL,
-    TRANSLATE_DATE_TIME   TIMESTAMP    NOT NULL
+    lt_translate_data_key SERIAL PRIMARY KEY,
+    user_id               VARCHAR(255) NOT NULL,
+    document_name         VARCHAR(255) NOT NULL,
+    text_length           INT4         NOT NULL,
+    translate_date_time   TIMESTAMP    NOT NULL
 );
