f46bd8b3d1b58b279e674e365e0efe9270791139
Anatolii Gurskyi <anatolii.gurskyi@avenga.com>
2021-03-09 18:38:57 +0200

    [LTS-105] RDS password rotation

---
+++ src/main/java/com/avenga/fil/lt/constant/Profiles.java
@@ -0,0 +1,10 @@
+package com.avenga.fil.lt.constant;
+
+public final class Profiles {
+
+    public static final String NOT_DB = "!db";
+
+    private Profiles() {
+        throw new IllegalStateException("Instance of this class can't be initialized!");
+    }
+}
